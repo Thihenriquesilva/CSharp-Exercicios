d812b425dd3f83ae797836bf29064e09ccb61a6f
Thihenriquesilva <henriquesilva322@outlook.com>
2019-09-20 11:04:30 -0300

    aula 2

---
+++ num/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = 0;
+
+        
+        do{
+            Console.WriteLine("Digite um numero");
+            p = int.Parse(Console.ReadLine());
+
+            if(p != 0)
+            {
+                if(p % 2 == 0){
+                    Console.WriteLine("Este número é par!");
+
+                }
+                else{
+                    Console.WriteLine("Este número é impar!");
+                }
+            }
+        }while(p != 0);
+        }
+    }
+}
