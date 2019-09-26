4ab6e15c0816041918fc9ec0e16e981e0a051b1f
Thiago Henrique <henriquesilva322@outlook.com>
2019-09-25 22:21:58 -0300

    arrays

--- matriz/Program.cs
+++ matriz/Program.cs
@@ -6,7 +6,20 @@ namespace matriz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] lista;
+
+            lista = new string[4];
+
+            lista[0] = "Thiago";
+            lista[1] = "Henrique";
+            lista[2] = "da";
+            lista[3] = "Silva";
+
+            for (int i=0; i<5; i++){
+                Console.Write(lista[i]);
+                Console.Write("");
+            }
+            Console.ReadKey();
         }
     }
 }
