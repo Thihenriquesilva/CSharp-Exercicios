5f1cc4f1f12b0344743d01e43bfb51f701aee38b
Thihenriquesilva <henriquesilva322@outlook.com>
2019-09-27 11:17:01 -0300

    POO

---
+++ mcbonalds/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace mcbonalds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente1 = new Cliente("Thiago","55 11 5555-5555","thiago@email.com");
+
+            Console.WriteLine("Nome: " + cliente1.Nome);
+            Console.WriteLine("Telefone" + cliente1.Telefone);
+            Console.WriteLine("Email: " + cliente1.Email);
+        }
+    }
+}
