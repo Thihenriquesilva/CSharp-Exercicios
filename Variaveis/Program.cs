2c988123574e3dbae539caa177ef4ed1b100a77f
Thiago Henrique <henriquesilva322@outlook.com>
2019-10-06 22:36:20 -0300

    variaveis

---
+++ Variaveis/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         //*Conversões implicita */   	
+        // int numero = 1234;
+        // decimal numero2 = numero;
+        // System.Console.WriteLine($"{numero}");
+        // System.Console.WriteLine($"{numero2}");
+
+        // //*Conversões explicitas */
+        // double num = 123.4;
+        // int num2;
+        // num2 = (int)num;
+        
+        //*Conversão teste */
+
+        int num;
+        num = Int32.Parse("123");
+ 
+        System.Console.WriteLine(num.ToString());
+
+
+        }
+    }
+}
